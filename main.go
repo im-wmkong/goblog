2810adf5498d27c9c12bdc196798ddd4745f9232
WmKong <im.wmkong@gmail.com>
2020-11-16 23:08:59 +0800

    新增关于页面

--- main.go
+++ main.go
@@ -6,7 +6,13 @@ import (
 )
 
 func handlerFuc(w http.ResponseWriter, r *http.Request)  {
-	fmt.Fprint(w, "h1>Hello, 这里是 goblog</h1>");
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
+	} else if r.URL.Path == "/about" {
+		fmt.Fprint(w, "请求路径为："+r.URL.Path)
+	} else {
+		fmt.Fprint(w, "页面未找到")
+	}
 }
 
 func main()  {
