39af709a611c8282a60c6ea199e3d2b774f6f422
WmKong <im.wmkong@gmail.com>
2020-11-18 00:07:15 +0800

    集成 Gorilla Mux 路由

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
-func defaultHandler(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog！</h1>")
@@ -22,9 +23,43 @@ func aboutHandler(w http.ResponseWriter, r *http.Request) {
 		"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
 }
 
+func notFoundHandler(w http.ResponseWriter, r *http.Request)  {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
+		"<p>如有疑惑，请联系我们。</p>")
+}
+
+func aritlcesShowHandler(w http.ResponseWriter, r *http.Request)  {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	fmt.Fprint(w, "文章 ID：" + id)
+}
+
+func aritlcesIndexHandler(w http.ResponseWriter, r *http.Request)  {
+	fmt.Fprint(w,"访问文章列表")
+}
+
+func aritlcesStoreHandler(w http.ResponseWriter, r *http.Request)  {
+	fmt.Fprint(w, "创建新的文章")
+}
+
 func main()  {
-	router := http.NewServeMux()
-	router.HandleFunc("/", defaultHandler)
-	router.HandleFunc("/about", aboutHandler)
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
+	router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
+
+	router.HandleFunc("/articles/{id:[0-9]+}", aritlcesShowHandler).Methods("GET").Name("articles.show")
+	router.HandleFunc("/articles", aritlcesIndexHandler).Methods("GET").Name("articles.index")
+	router.HandleFunc("/articles", aritlcesStoreHandler).Methods("POST").Name("articles.store")
+
+	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
+
+	homeURL, _ := router.Get("home").URL()
+	fmt.Println("homeURL: ", homeURL)
+	articleURL, _ := router.Get("articles.show").URL("id", "23")
+	fmt.Println("articleURL: ", articleURL)
+
 	http.ListenAndServe(":3000", router)
 }
