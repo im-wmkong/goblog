fb238d833be56b692b671b5dc10645c93e06aeb2
WmKong <im.wmkong@gmail.com>
2020-11-21 10:12:57 +0800

    文章列表

--- main.go
+++ main.go
@@ -30,6 +30,15 @@ type Article struct {
 	ID          int64
 }
 
+func (a Article) Link() string {
+	showURL, err := router.Get("articles.show").URL("id", strconv.FormatInt(a.ID, 10))
+	if err != nil {
+		checkError(err)
+		return ""
+	}
+	return showURL.String()
+}
+
 func initDB() {
 	var err error
 	config := mysql.Config{
@@ -143,7 +152,25 @@ func aritlcesShowHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func aritlcesIndexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "访问文章列表")
+	rows, err := db.Query("SELECT * from articles")
+	checkError(err)
+	defer rows.Close()
+
+	var articles []Article
+	for rows.Next() {
+		var article Article
+		err := rows.Scan(&article.ID, &article.Title, &article.Body)
+		checkError(err)
+		articles = append(articles, article)
+	}
+
+	err = rows.Err()
+	checkError(err)
+
+	tmpl, err := template.ParseFiles("resources/views/articles/index.gohtml")
+	checkError(err)
+
+	tmpl.Execute(w, articles)
 }
 
 func aritlcesCreateHandler(w http.ResponseWriter, r *http.Request) {
