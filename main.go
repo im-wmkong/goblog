ae8d54fef77226b2077ffbf6852203a634c456ca
WmKong <im.wmkong@gmail.com>
2020-11-28 00:30:11 +0800

    配置和环境变量

--- main.go
+++ main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"goblog/app/http/middlewares"
 	"goblog/bootstrap"
+	"goblog/config"
+	c "goblog/pkg/config"
 	"net/http"
 )
+
+func init()  {
+	config.Initialize()
+}
 
 func main() {
 	bootstrap.SetupDB()
 	router := bootstrap.SetupRoute()
 
-	http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
+	http.ListenAndServe(":"+c.GetString("app.port"), middlewares.RemoveTrailingSlash(router))
 }
